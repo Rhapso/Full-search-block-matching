4f188201115257d17fd1840729d6431ba9acbea9
Rhapso <your_mr_fan@sjtu.edu.cn>
2018-03-29 14:29:48 +0800

    simple start ctr

--- ctr.v
+++ ctr.v
@@ -17,17 +17,35 @@
 ***************************************************************** 
   ** Copyright (c) notice  
 *****************************************************************/ 
-module ctr(
+module ctr
+#(
+  parameter   WORD_WIDETH       8
+)
+(
     input                       clk,
     input                       turnenable,
-    output reg  [3:0]           select4bit
-)
+    input                       rst_n,
+    output reg  [3:0]           ctr_word,
+    output reg                  mem_en_input,
+    output reg                  mem_init_mode
+);
+reg [4:0]   counter_24;
 
-reg [19:0]  outputReg;
-
-always @ (clk)
+always @ (posedge clk)
 begin
-  
+    if(!rst_n && counter_24 != 5'b10111) counter_24 <= counter_24 + 5'b00001;
+    else counter_24 <= 5'b0;
 end
+
+always @ (posedge clk)
+begin
+    case(counter_24)
+    5'b00000:
+    begin
+        ctr_word <= 5'b00000;
+        mem_en_input <= 1'b0;
+        mem_init_mode <= 1'b0;
+    end
+
 
 endmodule
