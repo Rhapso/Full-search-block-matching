a4ccfc76e7c20a4de4f0652471e179ce0f1b351f
yangbo <g942190897@gmail.com>
2018-04-10 15:29:35 +0800

    fix mistakes: 1.compare.v中的enable信号之前未使用 2.在PE.v和compare.v中使用阻塞赋值

--- PE.v
+++ PE.v
@@ -51,92 +51,92 @@ begin
 	begin
 
 		if(a00>b00)
-			abs_value[0] = a00 - b00;
+			abs_value[0] <= a00 - b00;
 		else
-			abs_value[0] = b00 - a00;
+			abs_value[0] <= b00 - a00;
 		if(a01>b01)
-			abs_value[1] = a01 - b01;
+			abs_value[1] <= a01 - b01;
 		else
-			abs_value[1] = b01 - a01;
+			abs_value[1] <= b01 - a01;
 		if(a02>b02)
-			abs_value[2] = a02 - b02;
+			abs_value[2] <= a02 - b02;
 		else
-			abs_value[2] = b02 - a02;
+			abs_value[2] <= b02 - a02;
 		if(a03>b03)
-			abs_value[3] = a03 - b03;
+			abs_value[3] <= a03 - b03;
 		else
-			abs_value[3] = b03 - a03;
+			abs_value[3] <= b03 - a03;
 		
 		if(a10>b10)
-			abs_value[4] = a10 - b10;
+			abs_value[4] <= a10 - b10;
 		else
-			abs_value[4] = b10 - a10;
+			abs_value[4] <= b10 - a10;
 		if(a11>b11)
-			abs_value[5] = a11 - b11;
+			abs_value[5] <= a11 - b11;
 		else
-			abs_value[5] = b11 - a11;
+			abs_value[5] <= b11 - a11;
 		if(a12>b12)
-			abs_value[6] = a12 - b12;
+			abs_value[6] <= a12 - b12;
 		else
-			abs_value[6] = b12 - a12;
+			abs_value[6] <= b12 - a12;
 		if(a13>b13)
-			abs_value[7] = a13 - b13;
+			abs_value[7] <= a13 - b13;
 		else
-			abs_value[7] = b13 - a13;
+			abs_value[7] <= b13 - a13;
 
 		if(a20>b20)
-			abs_value[8] = a20 - b20;
+			abs_value[8] <= a20 - b20;
 		else
-			abs_value[8] = b20 - a20;
+			abs_value[8] <= b20 - a20;
 		if(a21>b21)
-			abs_value[9] = a21 - b21;
+			abs_value[9] <= a21 - b21;
 		else
-			abs_value[9] = b21 - a21;
+			abs_value[9] <= b21 - a21;
 		if(a22>b22)
-			abs_value[10] = a22 - b22;
+			abs_value[10] <= a22 - b22;
 		else
-			abs_value[10] = b22 - a22;
+			abs_value[10] <= b22 - a22;
 		if(a23>b23)
-			abs_value[11] = a23 - b23;
+			abs_value[11] <= a23 - b23;
 		else
-			abs_value[11] = b23 - a23;
+			abs_value[11] <= b23 - a23;
 
 		if(a30>b30)
-			abs_value[12] = a30 - b30;
+			abs_value[12] <= a30 - b30;
 		else
-			abs_value[12] = b30 - a30;
+			abs_value[12] <= b30 - a30;
 		if(a31>b31)
-			abs_value[13] = a31 - b31;
+			abs_value[13] <= a31 - b31;
 		else
-			abs_value[13] = b31 - a31;
+			abs_value[13] <= b31 - a31;
 		if(a32>b32)
-			abs_value[14] = a32 - b32;
+			abs_value[14] <= a32 - b32;
 		else
-			abs_value[14] = b32 - a32;
+			abs_value[14] <= b32 - a32;
 		if(a33>b33)
-			abs_value[15] = a33 - b33;
+			abs_value[15] <= a33 - b33;
 		else
-			abs_value[15] = b33 - a33;
+			abs_value[15] <= b33 - a33;
 
-		sum1[0] = abs_value[0] + abs_value[15];
-		sum1[1] = abs_value[1] + abs_value[14];
-		sum1[2] = abs_value[2] + abs_value[13];
-		sum1[3] = abs_value[3] + abs_value[12];
-		sum1[4] = abs_value[4] + abs_value[11];
-		sum1[5] = abs_value[5] + abs_value[10];
-		sum1[6] = abs_value[6] + abs_value[9];
-		sum1[7] = abs_value[7] + abs_value[8];
-		sum1[8] = abs_value[8] + abs_value[7];
+		sum1[0] <= abs_value[0] + abs_value[15];
+		sum1[1] <= abs_value[1] + abs_value[14];
+		sum1[2] <= abs_value[2] + abs_value[13];
+		sum1[3] <= abs_value[3] + abs_value[12];
+		sum1[4] <= abs_value[4] + abs_value[11];
+		sum1[5] <= abs_value[5] + abs_value[10];
+		sum1[6] <= abs_value[6] + abs_value[9];
+		sum1[7] <= abs_value[7] + abs_value[8];
+		sum1[8] <= abs_value[8] + abs_value[7];
 
-		sum2[0] = sum1[0] + sum1[7];
-		sum2[1] = sum1[1] + sum1[6];
-		sum2[2] = sum1[2] + sum1[5];
-		sum2[3] = sum1[3] + sum1[4];
+		sum2[0] <= sum1[0] + sum1[7];
+		sum2[1] <= sum1[1] + sum1[6];
+		sum2[2] <= sum1[2] + sum1[5];
+		sum2[3] <= sum1[3] + sum1[4];
 
-		sum3[0] = sum2[0] + sum2[3];
-		sum3[1] = sum2[1] + sum2[2];
+		sum3[0] <= sum2[0] + sum2[3];
+		sum3[1] <= sum2[1] + sum2[2];
 
-		sum = sum3[0] + sum3[1];
+		sum <= sum3[0] + sum3[1];
 	end
 end
 
--- compare.v
+++ compare.v
@@ -2,22 +2,22 @@ module compare(
 	input clk,
 	input enable,
 
-	input [11:0] sum0,
-	input [11:0] sum1,
-	input [11:0] sum2,
-	input [11:0] sum3,
-	input [11:0] sum4,
-	input [11:0] sum5,
-	input [11:0] sum6,
-	input [11:0] sum7,
-	input [11:0] sum8,
-	input [11:0] sum9,
-	input [11:0] sum10,
-	input [11:0] sum11,
-	input [11:0] sum12,
-	input [11:0] sum13,
-	input [11:0] sum14,
-	input [11:0] sum15,
+	input reg [11:0] sum0,
+	input reg [11:0] sum1,
+	input reg [11:0] sum2,
+	input reg [11:0] sum3,
+	input reg [11:0] sum4,
+	input reg [11:0] sum5,
+	input reg [11:0] sum6,
+	input reg [11:0] sum7,
+	input reg [11:0] sum8,
+	input reg [11:0] sum9,
+	input reg [11:0] sum10,
+	input reg [11:0] sum11,
+	input reg [11:0] sum12,
+	input reg [11:0] sum13,
+	input reg [11:0] sum14,
+	input reg [11:0] sum15,
 
 	input rst_n,
 	output reg [19:1] out
@@ -29,22 +29,22 @@ reg [11:0] sum [0:15];
 
 always @ (*)
 begin
-sum[0]=sum0;
-sum[1]=sum1;
-sum[2]=sum2;
-sum[3]=sum3;
-sum[4]=sum4;
-sum[5]=sum5;
-sum[6]=sum6;
-sum[7]=sum7;
-sum[8]=sum8;
-sum[9]=sum9;
-sum[10]=sum10;
-sum[11]=sum11;
-sum[12]=sum12;
-sum[13]=sum13;
-sum[14]=sum14;
-sum[15]=sum15;
+	sum[0] = sum0;
+	sum[1] = sum1;
+	sum[2] = sum2;
+	sum[3] = sum3;
+	sum[4] = sum4;
+	sum[5] = sum5;
+	sum[6] = sum6;
+	sum[7] = sum7;
+	sum[8] = sum8;
+	sum[9] = sum9;
+	sum[10] = sum10;
+	sum[11] = sum11;
+	sum[12] = sum12;
+	sum[13] = sum13;
+	sum[14] = sum14;
+	sum[15] = sum15;
 end
 
 integer i;
@@ -52,37 +52,37 @@ integer c;
 
 always @ (posedge clk)
 begin
-	mad = sum[0];
+	mad <= sum[0];
 	c = 0;
-	if(rst_n)
+	if(rst_n or enable)
 	begin
 		for(i=1;i<16;i=i+1)
 		begin
 			if(mad > sum[i])
-				mad = sum[i];
+				mad <= sum[i];
 				c = i;
 		end
 		case(c)
-		0:	mv = 8'h00;
-		1:	mv = 8'h01;
-		2:	mv = 8'h02;
-		3:	mv = 8'h03;
-		4:	mv = 8'h10;
-		5:	mv = 8'h11;
-		6:	mv = 8'h12;
-		7:	mv = 8'h13;
-		8:	mv = 8'h20;
-		9:	mv = 8'h21;
-		10:	mv = 8'h22;
-		11:	mv = 8'h23;
-		12:	mv = 8'h30;
-		13:	mv = 8'h31;
-		14:	mv = 8'h32;
-		15:	mv = 8'h33;
+		0:	mv <= 8'h00;
+		1:	mv <= 8'h01;
+		2:	mv <= 8'h02;
+		3:	mv <= 8'h03;
+		4:	mv <= 8'h10;
+		5:	mv <= 8'h11;
+		6:	mv <= 8'h12;
+		7:	mv <= 8'h13;
+		8:	mv <= 8'h20;
+		9:	mv <= 8'h21;
+		10:	mv <= 8'h22;
+		11:	mv <= 8'h23;
+		12:	mv <= 8'h30;
+		13:	mv <= 8'h31;
+		14:	mv <= 8'h32;
+		15:	mv <= 8'h33;
 		endcase
 	end
 
-	out = {mad, mv}
+	out <= {mad, mv}
 end
 
 endmodule
